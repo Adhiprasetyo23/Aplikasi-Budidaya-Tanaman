acd6c740b79cc300c0e8c7318671d30673517868
Asus\adhiprastyo <190103031@fikom.udb.ac.id>
2022-07-11 19:09:29 +0700

    penambahan plugin

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+   await Hive.initFlutter();
+  var box = await Hive.openBox('user');
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
